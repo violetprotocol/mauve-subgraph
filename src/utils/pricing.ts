76e9d6491b53c6f7bc2aa285693e221817151a81
ra-phael <10075759+ra-phael@users.noreply.github.com>
2023-09-08 11:20:22 +0100

    update pricing util

--- src/utils/pricing.ts
+++ src/utils/pricing.ts
@@ -4,8 +4,17 @@ import { Bundle, Pool, Token } from '../types/schema'
 import { BigDecimal, BigInt, dataSource } from '@graphprotocol/graph-ts'
 import { exponentToBigDecimal, safeDiv } from './index'
 
-const WETH_ADDRESS_OP_GOERLI = '0x4200000000000000000000000000000000000006'
-const WETH_ADDRESS_MAINNET = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+// Optimism Goerli
+const WETH_OP_GOERLI = '0x4200000000000000000000000000000000000006'
+const USDC_OP_GOERLI = '0x7E07E15D2a87A24492740D16f5bdF58c16db0c4E'
+const OUT_1 = '0x32307adfFE088e383AFAa721b06436aDaBA47DBE'
+const OUT_2 = '0xb378eD8647D67b5dB6fD41817fd7a0949627D87a'
+// Mainnet
+const WETH_MAINNET = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+const USDC_MAINNET = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
+const LIDO_STAKED_ETH =  '0xae7ab96520de3a18e5e111b5eaab095312d7fe84'
+const LIQUID_STAKED_ETH = '0x8c1BEd5b9a0928467c9B1341Da1D7BD5e10b6549'
+const EUROC = '0x1aBaEA1f7C830bD89Acc67eC4af516284b1bC33c'
 
 const USDC_WETH_POOL = {
   'optimism-goerli': '0xD7b15F685D998A7e692b98E9A59547b7B35BC92a',
@@ -15,9 +24,17 @@ const USDC_WETH_POOL = {
 // token where amounts should contribute to tracked volume and liquidity
 // usually tokens that many tokens are paired with s
 export let WHITELIST_TOKENS: string[] = [
-  WETH_ADDRESS_OP_GOERLI,
-  WETH_ADDRESS_MAINNET,
-  '0x07865c6E87B9F70255377e024ace6630C1Eaa37F', // USDC
+  // Mainnet
+  WETH_MAINNET,
+  USDC_MAINNET,
+  LIDO_STAKED_ETH,
+  LIQUID_STAKED_ETH,
+  EUROC,
+  // Optimism Goerli
+  OUT_1,
+  OUT_2,
+  USDC_OP_GOERLI,
+  WETH_OP_GOERLI,
   '0x326C977E6efc84E512bB9C30f76E30c160eD06FB' // LINK
   // '0x6b175474e89094c44da98b954eedeac495271d0f', // DAI
   // '0xdac17f958d2ee523a2206206994597c13d831ec7', // USDT
@@ -40,7 +57,8 @@ export let WHITELIST_TOKENS: string[] = [
 ]
 
 let STABLE_COINS: string[] = [
-  '0x07865c6E87B9F70255377e024ace6630C1Eaa37F'
+  USDC_MAINNET,
+  USDC_OP_GOERLI
   // '0x6b175474e89094c44da98b954eedeac495271d0f',
   // '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
   // '0xdac17f958d2ee523a2206206994597c13d831ec7',
@@ -87,7 +105,7 @@ export function getEthPriceInUSD(): BigDecimal {
  * @todo update to be derived ETH (add stablecoin estimates)
  **/
 export function findEthPerToken(token: Token): BigDecimal {
-  if (token.id == WETH_ADDRESS_OP_GOERLI || token.id == WETH_ADDRESS_MAINNET) {
+  if (token.id == WETH_OP_GOERLI || token.id == WETH_MAINNET) {
     return ONE_BD
   }
   let whiteList = token.whitelistPools
