8375cd77deb631bd37aa4558f3fc32f46442dc4c
ra-phael <10075759+ra-phael@users.noreply.github.com>
2023-09-08 10:47:37 +0100

    consistency

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,7 +4,7 @@ import { Factory as FactoryContract } from '../types/templates/Pool/Factory'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
 export const FACTORY_ADDRESSES = {
-  mainnet: '0x0569168709a869e7f4Ba142c49BFF7faA14f76C8',
+  'mainnet': '0x0569168709a869e7f4Ba142c49BFF7faA14f76C8',
   'optimism-goerli': '0xEF8894089c991CF9c009Cd66F7F2fb9dC0e2fB19'
 }
 export let ZERO_BI = BigInt.fromI32(0)
@@ -14,6 +14,6 @@ export let ONE_BD = BigDecimal.fromString('1')
 export let BI_18 = BigInt.fromI32(18)
 
 export let factoryContracts = {
-  mainnet: FactoryContract.bind(Address.fromString(FACTORY_ADDRESSES.mainnet)),
+  'mainnet': FactoryContract.bind(Address.fromString(FACTORY_ADDRESSES['mainnet'])),
   'optimism-goerli': FactoryContract.bind(Address.fromString(FACTORY_ADDRESSES['optimism-goerli']))
 }
